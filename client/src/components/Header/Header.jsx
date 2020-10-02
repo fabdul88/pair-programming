4f7c6fb40845cec416587dfb7d81bd09d76d0169
abdul farhan <kukluu@hotmail.com>
2020-10-02 15:47:42 -0700

    added some List styling | added logo image

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react";
+import ambulance from "../../assets/ambulance.svg";
 
 const Header = () => {
-    return (
-        <div className="header">
-            <div className="header__logo">
-                Ambulance App
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="header">
+      <div className="header__div">
+        <img className="header__logo-image" src={ambulance} alt="" />
+      </div>
+      <div className="header__logo">Ambulance App</div>
+    </div>
+  );
+};
 
 export default Header;
--- client/src/components/ReportList/ReportList.jsx
+++ client/src/components/ReportList/ReportList.jsx
@@ -10,14 +10,18 @@ function ReportList(props) {
         <div className="report__body">
           <div className="report__card">
             <div className="report__listItem">
-              <label>Call Type</label>
-              <div>
+              <div className="report__back">
                 {props.updatedList.map((item) => {
                   return (
                     <>
-                      <p>{item.address}</p>
-                      <p>{item.time}</p>
-                      <p>{item.ambulanceArrivalTime}</p>
+                      <label className="report__label">ADDRESS</label>
+                      <p className="report__p">{item.address}</p>
+                      <label className="report__label">CALL TIME</label>
+                      <p className="report__p">{item.time}</p>
+                      <label className="report__label">
+                        AMBULANCE ARRIVAL TIME
+                      </label>
+                      <p className="report__p">{item.ambulanceArrivalTime}</p>
                     </>
                   );
                 })}
